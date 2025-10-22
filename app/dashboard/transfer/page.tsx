2b8b268b422c4eb042d46b71dca3f5076d7cd6cf
kenmin <kenmin.ho@sjsu.edu>
2025-10-22 15:39:05 -0700

    test fix merge:

--- app/dashboard/transfer/page.tsx
+++ app/dashboard/transfer/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "../components/app-sidebar";
-=======
->>>>>>> e5531728a5498afe0d0be01fe75495667d5594e2
 import { BalanceCard } from "../components/balance-card";
 import { TransferCard } from "./components/transfer-card";
 import { RecentTransfersCard } from "./components/recent-transfers-card";
@@ -15,7 +12,6 @@ type Customer = {
 };
 
 export default function TransferPage() {
-<<<<<<< HEAD
     const [selected, setSelected] = useState<string | null>(null);
      const [customer, setCustomer] = useState<Customer | null>(null);
 
@@ -44,30 +40,18 @@ export default function TransferPage() {
     
         fetchProfile();
       }, []);
-=======
-  const [selected, setSelected] = useState<string | null>(null);
->>>>>>> e5531728a5498afe0d0be01fe75495667d5594e2
 
   return (
     <>
       <div className="p-10 space-y-6">
         <h1 className="text-4xl font-bold">Transfer</h1>
 
-<<<<<<< HEAD
             <div className="grid grid-cols-2 gap-6">
               {/* Left Column */}
               <div className="flex flex-col gap-6 w-fit">
                 <div className="flex-1">
                   <BalanceCard userBalance={customer?.balance ?? 0} />
                 </div>
-=======
-        <div className="grid grid-cols-2 gap-6">
-          {/* Left Column */}
-          <div className="flex flex-col gap-6 w-fit">
-            <div className="flex-1">
-              <BalanceCard userBalance={1000} />
-            </div>
->>>>>>> e5531728a5498afe0d0be01fe75495667d5594e2
 
             <div className="flex-1">
               <RecentTransfersCard onSelect={setSelected} />
