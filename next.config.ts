64f81c16f600adecc696a13488ddf643cbdb522e
Sarthakischauhan <sarthakchauhann@gmail.com>
2025-11-24 15:11:15 -0800

    dockerfile and docker compose

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,9 @@ const nextConfig = {
       "/api/**/*": ["./node_modules/**/*.wasm", "./node_modules/**/*.proto"],
     },
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
