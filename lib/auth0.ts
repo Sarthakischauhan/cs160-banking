a1c22d33ca505b68f344324c7cf91012842c3a7c
Sarthakischauhan <sarthakchauhann@gmail.com>
2025-11-24 01:26:26 -0800

    fixed build errors, removed deprecated packages

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,21 +1,33 @@
-import { Auth0Client} from "@auth0/nextjs-auth0/server";
+import { Auth0Client } from "@auth0/nextjs-auth0/server";
 import { Session } from "@/types/session";
 
-import jwt from "jsonwebtoken"
+import { jwtDecode } from "jwt-decode";
 
 export const auth0 = new Auth0Client();
 
-export const getRole = (session: Session) : string => {
-  if (!session){
-    return ""
+export const getRole = (session: Session): string => {
+  if (!session) {
+    return "";
   }
-  const decodedToken = jwt.decode(session.tokenSet.idToken || "") as Record<string, any> | null;
-  const roles = decodedToken && decodedToken['https://my-app.example.com/roles'];
+  
+  const idToken = session.tokenSet.idToken || "";
 
-  if (!roles){
-    return ""
+  if (!idToken) {
+    return "";
   }
 
-  // assuming roles is an array, we return all roles comma seperated
-  return roles.join()
-}
+  const decodedToken = jwtDecode(idToken) as Record<string, any>;
+  
+  if (!decodedToken) {
+    return "";
+  }
+  
+  const roles = decodedToken["https://my-app.example.com/roles"];
+
+  if (!roles) {
+    return "";
+  }
+
+
+  return roles.join();
+};
