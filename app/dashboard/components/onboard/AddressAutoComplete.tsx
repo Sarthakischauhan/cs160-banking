28d64f4b740f6e70dc4ae577e94f0354c5c49c98
Jason <jdoria702@gmail.com>
2025-12-08 16:50:54 -0800

    locate button fixed

--- app/dashboard/components/onboard/AddressAutoComplete.tsx
+++ app/dashboard/components/onboard/AddressAutoComplete.tsx
@@ -16,6 +16,7 @@ export default function AddressAutocomplete({ setValue }: Props) {
     const init = async () => {
       const loader = new Loader({
         apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+        version: 'weekly',
         libraries: ["places"],
       });
 
--- components/ui/ChaseMap.tsx
+++ components/ui/ChaseMap.tsx
@@ -198,6 +198,7 @@ export default function ChaseMap() {
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
       version: 'weekly',
+      libraries: ['places'],
     });
 
     loader.load().then(() => {
